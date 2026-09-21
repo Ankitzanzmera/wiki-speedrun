f0acac1de0e86041b4024dfacf8f533bf6979417
Ankit Zanzmera <96160190+Ankitzanzmera@users.noreply.github.com>
2026-09-21 22:15:06 +0530

    Efficient Wikipedia Data Access (#2)
    * refactor(gitignore): uncomment .vscode to ignore .vscode folder while pushing code.
    
    * Implemented the baseline Wikipedia Speedrun solver. (#1)
    
    * feat(wikipedia-clinet): add client which fetch all valid link from given wikipedia page
    
    * feat(bfs): add basic bfs algorithm to reach target url
    
    * refactor(bfs): fix bug which was getting link from start url not current url
    
    * feat(wiki-client): add request session and cache to make wikipedia client more efficient and fast.
    
    * refactor(main): change target url to test code

---
+++ main.py
@@ -0,0 +1,20 @@
+import time
+from wiki_speedrun.bfs import BFS
+from wiki_speedrun.wikipedia import WikipediaClient
+
+
+START_URL = "https://en.wikipedia.org/wiki/Potato"
+TARGET_URL = "https://en.wikipedia.org/wiki/Barack_Obama"
+
+
+if __name__ == "__main__":
+    
+    bfs = BFS(wiki_client=WikipediaClient())
+    
+    start = time.time()
+    path = bfs.search(start_url=START_URL, target_url=TARGET_URL)
+    end = time.time()
+    
+    print(f"Time Taken to reach Target URL is {end-start} secs.")
+    
+    print(path)
---
+++ wiki_speedrun/bfs.py
@@ -0,0 +1,57 @@
+from collections import deque ## Using Double Endedn Queue, which use doubly linked structure
+from .wikipedia import WikipediaClient
+
+class BFS:
+    def __init__(self, wiki_client:WikipediaClient):
+        self.wiki_client = wiki_client
+    
+    def search(self, start_url, target_url):
+        
+        queue = deque([start_url])
+        visited = {start_url}
+        
+        parent = {
+            start_url:None
+        }
+        
+        while queue:
+            current_url = queue.popleft()
+            print(f"Visiting: {current_url}")
+
+            if current_url == target_url:
+                return self.reconstruct_path(parent=parent, target_url=target_url)
+            
+            links = self.wiki_client.get_all_valid_links(base_url=current_url)
+            
+            for link in links:
+                
+                if link in visited:
+                    continue
+                
+                visited.add(link)
+                
+                parent[link] = current_url
+                                
+                if link == target_url:
+                    return self.reconstruct_path(parent=parent, target_url=target_url)
+                    
+                queue.append(link)
+            
+        return None
+            
+
+    @staticmethod
+    def reconstruct_path(parent, target_url):
+        path = []
+        current = target_url
+
+        while current is not None:
+
+            path.append(current)
+
+            current = parent[current]
+
+        # We constructed target -> start, so reverse it.
+        path.reverse()
+
+        return path
